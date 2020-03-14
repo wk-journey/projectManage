dd75a0c019ba985f89afd1e8352807ec4042aa79
Kai <610288425@qq.com>
2020-03-14 18:57:15 +0800

    scss & postcss import

--- src/main.js
+++ src/main.js
@@ -3,14 +3,21 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
+import Cookies from 'js-cookie'
+
+import Element from "element-ui";
+
+import 'normalize.css/normalize.css'
+
+import '@/styles/index.scss' // global css
 
 import '@/permission'
 
 Vue.config.productionTip = false;
 
-Vue.use(ElementUI);
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+});
 
 new Vue({
   render: h => h(App),
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,11 +6,17 @@
       </div>
 
       <el-form-item label="用户名：">
-        <el-input v-model="loginForm.username" placeholder="请输入用户名"></el-input>
+        <el-input
+          v-model="loginForm.username"
+          placeholder="请输入用户名"
+        ></el-input>
       </el-form-item>
 
       <el-form-item label="密 码：">
-        <el-input v-model="loginForm.password" placeholder="请输入密码"></el-input>
+        <el-input
+          v-model="loginForm.password"
+          placeholder="请输入密码"
+        ></el-input>
       </el-form-item>
 
       <el-form-item>
@@ -31,24 +37,50 @@ export default {
       },
       redirect: undefined,
       otherQuery: {}
-    }
+    };
   },
   methods: {
     handleLogin() {
       this.$store
         .dispatch("user/login", this.loginForm)
         .then(res => {
-          this.$router.push({ path: this.redirect || '/', query: this.otherQuery })
+          this.$router.push({
+            path: this.redirect || "/",
+            query: this.otherQuery
+          });
           // .catch(err => {
           //   console.log(err)
           // })
         })
         .catch(err => {
-          console.log(err)
-        })
+          console.log(err);
+        });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+$cursor: #bbbbbb;
+
+// -webkit-mask:蒙板  cater-color：光标颜色
+@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
+  .login-container .el-input input {
+    color: $cursor;
+  }
+}
+
+/* reset element-ui css */
+.login-container {
+  .el-input {
+    display: inline-block;
+    height: 47px;
+    width: 85%;
+
+    input {
+      // 去除系统默认appearance的样式,常用于IOS下移除原生样式
+      -webkit-appearance: none;
     }
   }
 }
-</script>
-
-<style scope></style>
+</style>
