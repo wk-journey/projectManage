861392dee845523cbe496d57dfd8ced4790cf5ac
Kai <610288425@qq.com>
2020-03-14 23:09:47 +0800

    svg-icon init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 "use strict";
+const path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
@@ -8,9 +13,9 @@
 const port = process.env.port || process.env.npm_config_port || 9527; // dev port
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? './' : '/',
-  outputDir: 'dist',
-  assetsDir: 'static',
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  outputDir: "dist",
+  assetsDir: "static",
   lintOnSave: process.env.NODE_ENV === "development",
   productionSourceMap: false,
   devServer: {
@@ -27,7 +32,7 @@ module.exports = {
         changeOrigin: true,
         // ws: true,
         pathRewrite: {
-          ['^' + process.env.VUE_APP_BASE_API]: ''
+          ["^" + process.env.VUE_APP_BASE_API]: ""
         }
       }
     }
@@ -47,5 +52,23 @@ module.exports = {
         layout: "@/layout"
       }
     }
+  },
+  chainWebpack(config) {
+    // set svg-sprite-loader
+    config.module
+      .rule("svg")
+      .exclude.add(resolve("src/icons"))
+      .end();
+    config.module
+      .rule("icons")
+      .test("/.svg$/")
+      .include.add(resolve("src/icons"))
+      .end()
+      .use("svg-sprite-loader")
+      .loader("svg-sprite-loader")
+      .options({
+        symbolId: "icon-[name]"
+      })
+      .end();
   }
 };
