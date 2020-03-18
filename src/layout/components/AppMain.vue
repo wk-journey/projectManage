3c38b53969e173f69601f1c4a009f4c36c5151e3
Kai <610288425@qq.com>
2020-03-18 22:02:01 +0800

    app-main add

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,9 +1,28 @@
-<template></template>
+<template>
+  <section class="app-main">
+    <keep-alive>
+      <router-view :key="key" />
+    </keep-alive>
+  </section>
+</template>
 
 <script>
 export default {
-  name: "AppMain"
+  name: "AppMain",
+  computed: {
+    key() {
+      return this.$route.path
+    }
+  }
 }
 </script>
 
-<style scope></style>
+<style lang="scss" scope>
+.app-container {
+  /* 50= navbar  50  */
+  min-height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+}
+</style>
